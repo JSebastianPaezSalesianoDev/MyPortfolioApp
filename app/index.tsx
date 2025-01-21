6c51f34437343633fe3a2b85fd1f23fd2d99330b
JSebastianPaezSalesianoDev <usr3766@salesianos-lacuesta.net>
2025-01-21 19:42:59 +0000

    Feat(Login) Se hizo un login basico con dos TextInputs y un enlace a register

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Redirect } from "expo-router";
 
 const startPage = () => {
-  return <Redirect href="/welcomePage" />; // dogs
+  return <Redirect href="Login" />; // dogs, //welcomePage
 };
 
 export default startPage;
